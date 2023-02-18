c08bbed7d2e801efd107c7f205d689d4a2a15701
Ian326 <a01708940@tec.mx>
2023-02-18 14:42:00 -0600

    Added ex3

--- lab4/functions.js
+++ lab4/functions.js
@@ -27,3 +27,36 @@ function ex2(){
             alert("LoL. You're not just wrong, You're stupid. " + "And it took you: " + (d2-d1)/1000 + " seconds");
         }
 }
+
+function ex3(){
+    let breake = false;
+    let zero = 0, neg = 0, pos = 0; 
+    while(breake == false){
+        let x = prompt("Give me a number. Write x to continue")
+        if (x < 0) {
+            neg++       
+        }
+        else if (x == 0) {
+            zero++
+        }
+        else if (x > 0) {
+            pos++
+        }
+        else if (x == "x"){
+            breake = true;
+        }
+        else{
+            alert("Invalid input. Try again")
+        }
+    }
+    var table = document.getElementById("Ex3");
+        var row = table.insertRow(1);
+        var c1 = row.insertCell(0);
+        var c2 = row.insertCell(1);
+        var c3 = row.insertCell(2);
+        c1.innerHTML = neg;
+        c2.innerHTML = zero;
+        c3.innerHTML = pos;
+    var table = document.getElementById("Ex3").style.display = "inline-table";
+
+}
