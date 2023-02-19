daf34c10aef382cb3a4aa67755310b7e72baf5c0
Ian326 <a01708940@tec.mx>
2023-02-18 18:48:02 -0600

    UPDATED: ex2( ) Outputs text. ex3( ) checks for NaN. ex4( ) completed

--- lab4/functions.js
+++ lab4/functions.js
@@ -1,5 +1,5 @@
 function ex1(){
-    let x = prompt('Ingresa un numero entero mayor a 0: ');
+    let x = prompt('Give me an integer greater than 0: ');
     var table = document.getElementById("Ex1");
     for (let i = 1; i <= x; i++) {
         var row = table.insertRow(i);
@@ -10,7 +10,7 @@ function ex1(){
         c2.innerHTML = i**2;
         c3.innerHTML = i**3;
     };
-    var table = document.getElementById("Ex1").style.display = "inline-table";
+    document.getElementById("Ex1").style.display = "inline-table";
 };
 
 function ex2(){
@@ -22,25 +22,41 @@ function ex2(){
     let d2 = Date.now()
     if (x == z) {
             alert("LoL. You're right. "  + "And it only took you: " + (d2-d1)/1000 + " seconds");
+            document.getElementById("Ex2").innerHTML=
+            "CORRECT. User answered: '" + r1 + " + " + r2 + " = " + x +
+             "' in " + (d2-d1)/1000 + " seconds";
+            document.getElementById("Ex2").style.display="block";
         }
         else{
             alert("LoL. You're not just wrong, You're stupid. " + "And it took you: " + (d2-d1)/1000 + " seconds");
+            document.getElementById("Ex2").innerHTML=
+            "WRONG. User answered: '" + r1 + " + " + r2 + " = " + x +
+             "' in " + (d2-d1)/1000 + " seconds";
+            document.getElementById("Ex2").style.display="block";
         }
 }
 
 function ex3(){
     let breake = false;
-    let zero = -1, neg = 0, pos = 0;
+    let zero = 0, neg = 0, pos = 0;
     const array = [];
+
     while(breake == false){
         let x = prompt("Give me a number. Write x to continue")
         if(x == "x"){
             breake = true;
         }
         else{
-            array.push(x);
+            x = parseInt(x);
+            if (isNaN(x)){
+                alert("NaN. Try Again.") 
+            }
+            else{
+                array.push(x);
+            }
         }
     }
+
     for (let i = 0; i < array.length; i++) {
         if (array[i] < 0) {
             neg++
@@ -53,15 +69,16 @@ function ex3(){
         }
         else{}
     }
+
     var table = document.getElementById("Ex3");
-        var row = table.insertRow(1);
-        var c1 = row.insertCell(0);
-        var c2 = row.insertCell(1);
-        var c3 = row.insertCell(2);
-        c1.innerHTML = neg;
-        c2.innerHTML = zero;
-        c3.innerHTML = pos;
-    var table = document.getElementById("Ex3").style.display = "inline-table";
+    var row = table.insertRow(1);
+    var c1 = row.insertCell(0);
+    var c2 = row.insertCell(1);
+    var c3 = row.insertCell(2);
+    c1.innerHTML = neg;
+    c2.innerHTML = zero;
+    c3.innerHTML = pos;
+    document.getElementById("Ex3").style.display = "inline-table";
 }
 
 function ex4(){
@@ -78,6 +95,7 @@ function ex4(){
             breake = true;
         }
     }
+
     breake = false;
     while (breake == false) {
         row = prompt("Write number of rows")
@@ -93,6 +111,7 @@ function ex4(){
 
     const matrix = [];
     var r = [];
+    const avgs = [];
     var table = document.getElementById("Ex4");
     
     for (let i = 0; i < row; i++) {
@@ -105,7 +124,19 @@ function ex4(){
             r.push(rng);
         }
         matrix.push(r);
+
+        let rowAvg = 0;
+        for (let k = 0; k < r.length; k++){
+            rowAvg += r[k]; 
+        }
+        rowAvg = rowAvg/r.length;
+        avgs.push(rowAvg);
+
         r = [];
     }
+
+    
     document.getElementById("Ex4").style.display="inline-table";
+    document.getElementById("Ex4Avgs").innerHTML="Average per row: " + avgs.toString();
+    document.getElementById("Ex4Avgs").style.display="block";
 }
